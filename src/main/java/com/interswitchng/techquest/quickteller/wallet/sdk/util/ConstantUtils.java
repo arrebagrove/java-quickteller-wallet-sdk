6b518e536811dc6f62a96a06fcfba83a31bf7c30
Lekan.Omotayo <Lekan.Omotayo@Interswitchng.com>
2014-07-25 08:10:31 +0100

    IS-29

--- src/main/java/com/interswitchng/techquest/quickteller/wallet/sdk/util/ConstantUtils.java
+++ src/main/java/com/interswitchng/techquest/quickteller/wallet/sdk/util/ConstantUtils.java
@@ -4,7 +4,7 @@ public class ConstantUtils {
 
 	public static final String CARD_NAME = "default";
 	public static final String SECURE_HEADER = "4D";
-	public static final String SECURE_FORMAT_VERSION = "10";
+	public static final String SECURE_FORMAT_VERSION = "11";
 	public static final String SECURE_MAC_VERSION = "05";
 	public static final String SECURE_FOOTER = "5A";
 	public static final String SIGNATURE_HEADER = "Signature";
